6077519c9155402acb562af3e710d723d50d8827
Ehsan <ehsan5505>
2016-01-17 12:54:17 +0500

    design the first service in the asp namely tripController which provide the route for the get api/trips, output the test name

---
+++ App/Controllers/Api/TripController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Models
+{
+    public class TripController : Controller
+    {
+        [HttpGet("/api/trips")]
+        public JsonResult Get()
+        {
+            return Json(new { name = "Ehsan Rafeeque" });
+        }
+    }
+}
