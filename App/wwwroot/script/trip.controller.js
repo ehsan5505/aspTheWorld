b162628806c3f046a5587ca42fd748c57801ae50
Ehsan <ehsan5505>
2016-01-18 23:59:15 +0500

    add the angular with controller and module for app-trips in the tours view

---
+++ App/wwwroot/script/trip.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    //this is the module of the core
+    var app = angular.module("app-trips");
+
+    app.controller("tripController", tripController);
+
+    function tripController() {
+        var vm = this;
+        vm.name = "Ehsan involve in the test";
+    }
+
+})();
